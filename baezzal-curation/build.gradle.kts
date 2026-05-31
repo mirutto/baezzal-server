dd4f93ba4ce019a1c408c399d21c78a82c58b6b1
wwan13 <wwan13@naver.com>
2026-05-31 23:51:23 +0900

    refactor: swagger 설정을 config 모듈로 분리하고 upload 제거

--- baezzal-curation/build.gradle.kts
+++ baezzal-curation/build.gradle.kts
@@ -4,12 +4,12 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
-    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.13")
 
     implementation(project(":baezzal-config:jpa"))
     implementation(project(":baezzal-config:mysql"))
     implementation(project(":baezzal-config:redis"))
     implementation(project(":baezzal-config:oauth"))
+    implementation(project(":baezzal-config:swagger"))
     implementation(project(":baezzal-platform:cache"))
     implementation(project(":baezzal-platform:image"))
     implementation(project(":baezzal-platform:lock"))
